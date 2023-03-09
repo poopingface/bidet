4508689ca47178c57f5da4dea08cf9a3ecb64f89
AGIDADDY <127357121+AGIDADDY@users.noreply.github.com>
2023-03-09 13:17:18 -0800

    Add some release stuff (#1)
    * poo poo ka choo
    
    * poo poo ka choo
    
    * poo poo ka choo

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 
+
+def get_version() -> str:
+    rel_path = "bidet/__init__.py"
+    with open(rel_path, "r") as fp:
+        for line in fp.read().splitlines():
+            if line.startswith("__version__"):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='bidet',
-    version='0.0.1',
+    version=get_version(),
     description='The cleanest machine learning library',
     url='http://github.com/poopingface/bidet',
     author='Pooping Face',
