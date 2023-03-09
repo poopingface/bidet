732f2f89e6f9ceb01a191f847269f6805e277417
poopingfacebot <poopingface.co@gmail.com>
2023-03-08 19:39:34 -0500

    first version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bidet',
+    version='0.0.1',
+    description='The cleanest machine learning library',
+    url='http://github.com/poopingface/bidet',
+    author='Pooping Face',
+    author_email='poopingface.co@gmail.com',
+    license='MIT',
+    packages=['bidet'],
+    zip_safe=False
+)
